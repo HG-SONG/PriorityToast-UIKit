0b33f11d64f813557c5fbc02d69bfffc74685a34
SONG <geun2121@gmail.com>
2025-08-26 15:56:53 +0900

    Configure Demo App

--- PriorityToast/DemoApp/DemoApp/ViewController.swift
+++ PriorityToast/DemoApp/DemoApp/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import PriorityToast
 
 class ViewController: UIViewController {
-
+    private let prioirtyToastManager = PriorityToastManager.shared
+    
+    private let showToastButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Show Toast", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 18, weight: .medium)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        
+        setupButton()
     }
-
-
+    
+    private func setupButton() {
+        view.addSubview(showToastButton)
+        
+        // AutoLayout
+        NSLayoutConstraint.activate([
+            showToastButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            showToastButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            showToastButton.heightAnchor.constraint(equalToConstant: 44),
+            showToastButton.widthAnchor.constraint(equalToConstant: 160)
+        ])
+        
+        showToastButton.addTarget(self, action: #selector(showToastTapped), for: .touchUpInside)
+    }
+    
+    @objc private func showToastTapped() {
+        let toast = ToastItem(
+            message: "토스토스토스토스토스트",
+            priority: .highest
+        )
+        prioirtyToastManager.configure(policy: .discard)
+        prioirtyToastManager.show(toast)
+    }
 }
-
