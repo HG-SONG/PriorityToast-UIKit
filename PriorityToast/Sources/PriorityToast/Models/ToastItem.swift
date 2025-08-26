b574b9129a8d6de8740ed4a1b4374147bc626a47
SONG <geun2121@gmail.com>
2025-08-26 14:15:22 +0900

    ToastItem 추가

--- PriorityToast/Sources/PriorityToast/Models/ToastItem.swift
+++ PriorityToast/Sources/PriorityToast/Models/ToastItem.swift
@@ -5,3 +5,24 @@
 //  Created by SONG on 8/26/25.
 //
 
+import Foundation
+
+public struct ToastItem {
+    public let id = UUID()
+    public let message: String
+    public let priority: ToastPriority
+    public let duration: TimeInterval
+    public let view: ToastViewProtocol
+
+    public init(
+        message: String,
+        priority: ToastPriority,
+        duration: TimeInterval = 2.0,
+        view: ToastViewProtocol
+    ) {
+        self.message = message
+        self.priority = priority
+        self.duration = duration
+        self.view = view
+    }
+}
