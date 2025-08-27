9c2e7b633afbcb0dbf20f26abd6a5039594e8c16
SONG <geun2121@gmail.com>
2025-08-27 13:01:01 +0900

    Equatable 채택

--- PriorityToast/Sources/PriorityToast/Models/ToastItem.swift
+++ PriorityToast/Sources/PriorityToast/Models/ToastItem.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 @MainActor
-public struct ToastItem {
+public struct ToastItem: Equatable {
     public let id = UUID()
     public let message: String
     public let priority: ToastPriority
     public let duration: TimeInterval
     public let view: ToastViewProtocol
-
+    
     public init(
         message: String,
         priority: ToastPriority,
@@ -25,5 +25,9 @@ public struct ToastItem {
         self.priority = priority
         self.duration = duration
         self.view = view
+    }
+    
+    nonisolated public static func == (lhs: ToastItem, rhs: ToastItem) -> Bool {
+        return lhs.id == rhs.id
     }
 }
